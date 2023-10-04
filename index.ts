e29e92cf0bc217ae572ec21c4a6d6b5c783ef427
rk_quang-ht <quanght@rikkeisoft.com>
2023-10-04 19:32:06 +0700

    session 08 | function generic

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-const numbers: number[] = [1, 2, 3];
-const numbersWithGeneric: Array<number> = [1, 2, 3];
+function demoParams<K, V>(key: K, value: V): void {
+    console.log('key type:', typeof key)
+    console.log('value type:', typeof value)
+}
 
-const characters: string[] = ['a', 'b', 'c'];
-const charactersWithGeneric: Array<string> = ['a', 'b', 'c'];
+demoParams<string, number>('name', 1);
+demoParams<number, string>(1, 'abc');
 
-const promise: Promise<string> = new Promise<string>((resolve, reject) => {
-    resolve('123');
-});
+function demoReturnType<R>(out: R): R {
+    Number(out) + 1;
+    return out;
+}
+
+const result: number = demoReturnType<number>(2);
