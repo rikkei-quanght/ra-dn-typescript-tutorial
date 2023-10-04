9535726465de6f8e0bf3919c92c7513e011a3b73
rk_quang-ht <quanght@rikkeisoft.com>
2023-10-04 19:08:02 +0700

    session 08 | built-in generic types

--- index.ts
+++ index.ts
@@ -1,29 +1,9 @@
-const username : string = "tintt";
-const age : number = 29;
-const isActived : boolean = true;
-
-console.log('username', username);
-console.log('age', age);
-console.log('isActive', isActived);
-
-// Array
 const numbers: number[] = [1, 2, 3];
-const names : string[] = ["tintt", "thailq", "huynq"];
-const mixed: any[] = [1, "a", true, {}];
+const numbersWithGeneric: Array<number> = [1, 2, 3];
 
-// Function trả về number
-const calculatePriceWithTax = (price : number) : number => {
-    const rate : number = 7; // Thuế giả định: 7%
-    const tax = (price * rate / 100);
+const characters: string[] = ['a', 'b', 'c'];
+const charactersWithGeneric: Array<string> = ['a', 'b', 'c'];
 
-    return price + tax;
-}
-
-const showName = (name: string) : void => {
-    console.log(name)
-}
-
-showName("Giang")
-
-const priceWithTax : number = calculatePriceWithTax(1000);
-console.log(priceWithTax)
+const promise: Promise<string> = new Promise<string>((resolve, reject) => {
+    resolve('123');
+});
