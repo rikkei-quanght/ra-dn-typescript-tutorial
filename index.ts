5719fcbe29e75f91c944bda0a99d36d6cd94a317
rk_quang-ht <quanght@rikkeisoft.com>
2023-10-04 19:48:36 +0700

    session 08 | function generic R2

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import Admin from "./models/Admin";
+import Customer from "./models/Customer";
+import Product from "./models/Product";
+import User from "./models/User";
+
 function demoParams<K, V>(key: K, value: V): void {
     console.log('key type:', typeof key)
     console.log('value type:', typeof value)
@@ -12,3 +17,39 @@ function demoReturnType<R>(out: R): R {
 }
 
 const result: number = demoReturnType<number>(2);
+
+function printObject<T extends object>(obj: T): void {
+    console.log(obj);
+}
+
+const product: Product = {
+    product_id: 1
+};
+
+const user: User = {
+    user_id: 1,
+    name: 'sonlq'
+}
+
+const admin: Admin = {
+    user_id: 1,
+    name: 'sonlq',
+    isSystemAdmin: false
+}
+
+const customer: Customer = {
+    user_id: 1,
+    name: 'sonlq',
+    isActive: true,
+}
+
+printObject<Product>(product);
+
+function printUser<U extends User>(user: U): void {
+    console.log(user);
+}
+
+printUser<User>(user);
+printUser<Admin>(admin);
+printUser<Customer>(customer);
+// printUser<Product>(product);
---
+++ models/User.ts
@@ -0,0 +1,5 @@
+export default class User {
+    user_id: number;
+
+    name: string;
+}
