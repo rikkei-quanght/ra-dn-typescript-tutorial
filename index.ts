ea10aa4915a36f6f7f5e32003bab08bcc5e14b06
rk_quang-ht <quanght@rikkeisoft.com>
2023-10-09 20:34:51 +0700

    session 10 | property decorator

--- index.ts
+++ index.ts
@@ -1,52 +1,40 @@
-function Imutable(constructor: Function) {
-    console.log('this is Imutable decorator');
-    console.log(constructor);
-    Object.seal(constructor);
-    Object.seal(constructor.prototype);
+function Min(length: number) {
+    return function (constructor: Object, propertyName: string) {
+        let value: string;
+
+        const getValue = function () {
+            let now = new Date().toLocaleString('vi');
+            console.log(`Lấy ${propertyName} lúc ${now}`);
+            return value;
+        };
+
+        const setValue = function (newVal: string) {
+            value = newVal;
+            if (newVal.length < length) {
+                console.error(`${propertyName} ${newVal} ngắn quá, > ${length} ký tự`);
+            }
+        };
+
+        // Khai báo 2 method setter getter để kiểm soát 
+        Object.defineProperty(constructor, propertyName, {
+            get: getValue,
+            set: setValue
+        });
+    }
 }
 
-@Imutable
 class User {
-    id: number;
+    @Min(4)
+    public username: string;
 
-    name: string;
+    @Min(8)
+    public password: string;
 
-    constructor(id: number, name: string) {
-        this.id = id;
-        this.name = name;
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
     }
 }
 
-const user: User = new User(1, 'TinTT');
-Object.seal(user);
-user.name = 'a';
-delete user.name;
-
-console.log(user.name);
-
-// Example 2:
-function reportableClassDecorator<T extends { new(...args: any[]): {} }>(constructor: T) {
-    return class extends constructor {
-        reportingURL = "http://www...";
-    };
-}
-
-@reportableClassDecorator
-class BugReport {
-    type = "report";
-    title: string;
-
-    constructor(t: string) {
-        this.title = t;
-    }
-}
-
-const bug = new BugReport("Needs dark mode");
-console.log(bug.title); // Prints "Needs dark mode"
-console.log(bug.type); // Prints "report"
-
-// Note that the decorator _does not_ change the TypeScript type
-// and so the new property `reportingURL` is not known
-// to the type system:
-// bug.reportingURL;
-console.log(bug);
+let user1 = new User('tin', '123456');
+let user2 = new User('thailq', '12345678');
